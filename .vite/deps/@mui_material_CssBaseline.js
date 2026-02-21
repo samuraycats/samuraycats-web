1caa23d00d4cd36451b32a6178c64f30c5a1948e
Lorenzo Rascón <lorenzo.rascon@lefort.mx>
2026-02-20 21:50:31 -0600

    integracion

---
+++ .vite/deps/@mui_material_CssBaseline.js
@@ -0,0 +1,9 @@
+import {
+  CssBaseline_default
+} from "./chunk-HZOC2SYX.js";
+import "./chunk-ONIVJ5OZ.js";
+import "./chunk-X2SFOHQC.js";
+import "./chunk-WYYPSHNY.js";
+export {
+  CssBaseline_default as default
+};
